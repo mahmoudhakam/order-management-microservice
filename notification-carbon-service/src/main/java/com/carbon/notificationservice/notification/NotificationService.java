d745fe0a2d62210454c463f9509c00fcf95d7339
mahmoudhakam <maqamr.c@stc.com.sa>
2022-05-01 02:18:45 +0200

    add notification service

---
+++ notification-carbon-service/src/main/java/com/carbon/notificationservice/notification/NotificationService.java
@@ -0,0 +1,26 @@
+package com.carbon.notificationservice.notification;
+
+import com.carbon.notificationservice.notification.clients.CustomerServiceClient;
+import com.carbon.notificationservice.notification.domain.Invoice;
+import com.carbon.notificationservice.notification.domain.Shipment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+  private final CustomerServiceClient customerServiceClient;
+
+  public Mono<Void> notifyAboutCancellation(String customerId, String orderId, String message) {
+    return Mono.empty();
+  }
+
+  public Mono<Void> notifyAboutShipping(Shipment shipment) {
+    return Mono.empty();
+  }
+
+  public Mono<Void> notifyAboutPayment(Invoice invoice) {
+    return Mono.empty();
+  }
+}
---
+++ notification-carbon-service/src/main/java/com/carbon/notificationservice/notification/domain/Address.java
@@ -0,0 +1,19 @@
+package com.carbon.notificationservice.notification.domain;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import javax.validation.constraints.NotEmpty;
+
+@Value
+@Builder
+@RequiredArgsConstructor
+public class Address {
+  private final String line1;
+  private final String line2;
+  private final String city;
+  private final String county;
+  private final String postcode;
+  private final String country;
+}
